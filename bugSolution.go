efbfa7494d6ba2aa11090406bbdf523c17501619
BugHunter-X <askzenai@gmail.com>
2024-12-24 02:12:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var m map[string]int
+    if m == nil {
+        m = make(map[string]int)
+    }
+    m["hello"] = 1
+    fmt.Println(m["hello"])
+} 
